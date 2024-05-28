c55cdb917086d8cdf83d7fc7536cea7d288fdb2f
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2024-05-28 20:08:20 -0300

    Inserindo novo produto com POST

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -5,20 +5,14 @@ import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
-
     @Autowired
     private ProductService productService;
+
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return productService.findById(id);
@@ -27,5 +21,10 @@ public class ProductController {
     @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable) {
         return productService.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO productDTO) {
+        return productService.insert(productDTO);
     }
 }
