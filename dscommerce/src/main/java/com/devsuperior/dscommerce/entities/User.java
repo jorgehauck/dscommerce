970f41deac3abfcbb66c29f022f8a23b4307d700
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2024-07-13 18:12:51 -0300

    UserDetails, GrantedAuthority, not tested

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,13 +1,15 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -78,7 +80,6 @@ public class User {
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
@@ -99,7 +100,30 @@ public class User {
         }
         return false;
     }
-
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -109,7 +133,6 @@ public class User {
 
         return Objects.equals(id, user.id);
     }
-
     @Override
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
