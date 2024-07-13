f82d316ff38f79ac99719ff252222b50973aac62
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2024-07-13 18:02:15 -0300

    Domain model User Role, seed

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -21,6 +19,11 @@ public class User {
     private String password;
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
     public User() {}
 
     public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
@@ -81,6 +84,20 @@ public class User {
     }
     public List<Order> getOrders() {
         return orders;
+    }
+    public Set<Role> getRoles() {
+        return roles;
+    }
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
